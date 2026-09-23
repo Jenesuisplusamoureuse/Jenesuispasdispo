a1d8112e0fe3f9c6d1a43c4ae3c55d759a19f699
Jenesuisplusamoureuse <romanearbelet@gmail.com>
2026-09-23 15:59:06 +0200

    Add files via upload

---
+++ Blog/script.js
@@ -0,0 +1,14 @@
+const openOption = document.getElementById("open-option");
+const closeOption = documant.getElementById("close-option");
+const openOption = document.getElementById("overlay");
+
+function openOption() {
+    overlay.classlist.add("active");
+    
+}
+
+function openOption() {
+    overlay.classlist.remove("active");
+    
+}
+
